6afeb2a47db8fc4e591d2cc35339f5dc87a9e995
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2024-04-16 15:12:22 -0400

    feat: posts crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_172148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "subtitle"
+  end
 
 end
